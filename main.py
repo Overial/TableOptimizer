00a1ca1ebf312acc80ba29525eb61dcce32bddff
Overial <dfjx2201@yandex.ru>
2022-07-05 23:16:11 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Overial
+# https://github.com/Overial
+
+# importing necessary modules
+import sys
+import openpyxl
+
+
+# main function
+def main() -> int:
+    print('initiated table optimization')
+
+if __name__ == '__main__':
+    sys.exit(main())
